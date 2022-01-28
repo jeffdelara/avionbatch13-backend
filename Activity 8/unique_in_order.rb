69537ef4f2863b9a89a085c9cc68e365f48bca98
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-01-28 19:31:28 +0800

    unqiue

--- Activity 8/unique_in_order.rb
+++ Activity 8/unique_in_order.rb
@@ -1,23 +1,28 @@
 def unique_in_order(iterable)
   if iterable.kind_of?(Array) 
-    iterable.uniq
-  elsif iterable.kind_of?(String)
-    str_arr = iterable.split("")
-    
-    result = []
-    str_arr.each do |char|
-      if result.length > 0
-        # check if the last item in array is equal to char
-        # push if its not
-        if result[result.length - 1] != char 
-          result.push(char)
-        end
-      else # no content on results yet, immediately push
-        result.push(char)
-      end
-    end
-    
-    result
-  end
-
+    return get_unique(iterable)
+  end 
+  
+  str_arr = iterable.split("")
+  return get_unique(str_arr)
 end
+
+def get_unique(arr)
+  result = []
+  arr.each do |c|
+    # Loop each one
+    # we want to know if the result is empty
+    # so we could just push it immediately and not compare
+    if result.length > 0
+      # check if the last item in array is equal to c
+      # push if its not
+      if result[result.length - 1] != c 
+        result.push(c)
+      end
+    else 
+      result.push(c)
+    end
+  end
+  result
+end
+
